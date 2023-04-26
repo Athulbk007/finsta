1c313da19ac1e0360bfaf0b3ea29db12304dddcf
Athul <bkathul84@gmail.com>
2023-04-26 10:49:54 +0530

    ok

--- finsta/myapp/models.py
+++ finsta/myapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-from random import sample
 
 # Create your models here.
 class UserProfile(models.Model):
@@ -22,9 +21,6 @@ class UserProfile(models.Model):
         all_profiles=UserProfile.objects.all().exclude(user=self.user)
         following_profiles=self.following.all()
         suggessions=set(all_profiles) - set(following_profiles)
-
-        if len(suggessions)>2:
-            return sample(list(suggessions),2)
         return suggessions    
     
 class Posts(models.Model):
